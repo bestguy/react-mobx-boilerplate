0409e5d3b3a128adee87f9308a7124da3bb945bf
Gary Thomas <garlos@gmail.com>
2018-05-12 11:22:55 -0700

    Update reps

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import fecha from 'fecha';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
-import { Button, Container, Col, Row, Table } from 'reactstrap';
+import { Button, ButtonToolbar, Card, CardBlock, Container, Col, Row, Table } from 'reactstrap';
 
 @observer
 export default class App extends Component {
@@ -23,13 +23,19 @@ export default class App extends Component {
     return (
       <Container className="p-3">
         <Row>
-          <Col>
-            <Button color="primary">Hello</Button>
-            <Button>World!</Button>
+          <Col sm={6} xs={12}>
+            <ButtonToolbar>
+              <Button color="primary">Hello</Button>
+              <Button>World!</Button>
+            </ButtonToolbar>
           </Col>
           <Col sm="3">
-            <h1>{fecha.format(this.time, 'h:mm:ssA')}</h1>
-            <h3 className="text-muted">{fecha.format(this.time, 'MMM Do, YYYY')}</h3>
+            <Card>
+              <CardBlock>
+                <h2>{fecha.format(this.time, 'h:mm:ssA')}</h2>
+                <h3 className="text-muted">{fecha.format(this.time, 'MMM Do, YYYY')}</h3>
+              </CardBlock>
+            </Card>
           </Col>
         </Row>
       </Container>
