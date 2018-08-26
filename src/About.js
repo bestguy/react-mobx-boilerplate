4c3469be6c97e9428c39618cb2785817584530f8
bestguy <gary@gary.local>
2018-08-26 14:04:17 -0700

    Update visuals

--- src/About.js
+++ src/About.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Icon from 'react-fontawesome';
 
 export default () => (
   <div>
-    <h1 className="border-dark border-bottom p-2">About</h1>
+    <h1 className="border-dark border-bottom p-2">
+      <Icon name="info-circle" className="text-info" /> About
+    </h1>
     <p>
       This is a boilerplate sample for React 16, Mobx 5, Bootstrap 4, and react-router 4.
     </p>
--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import fecha from 'fecha';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 import { Button, ButtonToolbar, Card, CardBody } from 'reactstrap';
+import Icon from 'react-fontawesome';
 
 @observer
 export default class Home extends React.Component {
@@ -21,8 +22,10 @@ export default class Home extends React.Component {
   render() {
     return (
       <div>
-        <h1 className="border-dark border-bottom p-2">Home</h1>
-        <Card>
+        <h1 className="border-dark border-bottom p-2">
+          <Icon name="home" className="text-info" /> Home
+        </h1>
+        <Card className="mb-3">
           <CardBody>
             <h2>{fecha.format(this.time, 'h:mm:ssA')}</h2>
             <h3 className="text-muted">{fecha.format(this.time, 'MMM Do, YYYY')}</h3>
@@ -30,7 +33,7 @@ export default class Home extends React.Component {
         </Card>
 
         <ButtonToolbar>
-          <Button color="primary">Hello</Button>
+          <Button color="primary" className="mr-1">Hello</Button>
           <Button>World!</Button>
         </ButtonToolbar>
       </div>
