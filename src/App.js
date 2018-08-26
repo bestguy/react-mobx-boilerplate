858a095eaee7bfd93144871108a57a47d87d3bf8
bestguy <gary@gary.local>
2018-08-26 10:44:51 -0700

    Extract sidebar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-import { Col, ListGroup, Row } from 'reactstrap';
-import { NavLink, Route, withRouter } from 'react-router-dom';
-import Icon from 'react-fontawesome';
+import { Col, Row } from 'reactstrap';
+import { Route, withRouter } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-
-const ListGroupNav = ({ children, to }) => (
-  <NavLink
-    to={to}
-    exact
-    className="list-group-item"
-    activeClassName="active"
-  >
-    {children}
-  </NavLink>
-);
+import Sidebar from './Sidebar';
 
 @withRouter
 @observer
@@ -24,15 +13,8 @@ export default class App extends React.Component {
     return (
       <div>
         <Row className="no-gutters">
-          <Col xs={2} sm={3} >
-            <ListGroup flush>
-              <ListGroupNav to="/">
-                <Icon name="home" fixedWidth /> <span className="d-none d-sm-inline">Home</span>
-              </ListGroupNav>
-              <ListGroupNav to="/about">
-                <Icon name="info-circle" fixedWidth /> <span className="d-none d-sm-inline">About</span>
-              </ListGroupNav>
-            </ListGroup>
+          <Col xs={2} sm={3}>
+            <Sidebar />
           </Col>
           <Col xs={10} sm={9} className="p-3">
             <Route exact path="/" component={Home} />
---
+++ src/Sidebar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ListGroup } from 'reactstrap';
+import { NavLink, withRouter } from 'react-router-dom';
+import Icon from 'react-fontawesome';
+
+const ListGroupNav = ({ children, to }) => (
+  <NavLink
+    to={to}
+    exact
+    className="list-group-item"
+    activeClassName="active"
+  >
+    {children}
+  </NavLink>
+);
+
+@withRouter
+export default class Sidebar extends React.Component {
+  render() {
+    return (
+      <ListGroup flush>
+        <ListGroupNav to="/">
+          <Icon name="home" fixedWidth /> <span className="d-none d-sm-inline">Home</span>
+        </ListGroupNav>
+        <ListGroupNav to="/about">
+          <Icon name="info-circle" fixedWidth /> <span className="d-none d-sm-inline">About</span>
+        </ListGroupNav>
+      </ListGroup>
+    );
+  }
+}
