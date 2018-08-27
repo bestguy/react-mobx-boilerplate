990e8c0cf021139cfd03eb30d85e817aee1e463c
bestguy <gary@gary.local>
2018-08-26 21:07:26 -0700

    Add Clock

---
+++ src/components/Clock.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import range from 'lodash.range';
+
+const Clock = ({ width = 200, height = 200, time }) => {
+  const h = time.getHours();
+  const m = time.getMinutes();
+  const s = time.getSeconds();
+  const hour = h + m / 60;
+  const minute = m + s / 60;
+  const seconds = s + time.getMilliseconds() / 1000;
+
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox={`0 0 ${width} ${height}`}
+      preserveAspectRatio="xMinYMin meet"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g transform="translate(100, 100)">
+        <circle
+          cx={0}
+          cy={0}
+          r={99}
+          stroke="currentColor"
+          strokeWidth={1}
+          fill="none"
+        />
+        <line
+          x1={0}
+          y1={20}
+          x2={0}
+          y2={-60}
+          strokeWidth={6}
+          stroke="currentColor"
+          transform={`rotate(${hour * 30})`}
+        />
+        <line
+          x1={0}
+          y1={20}
+          x2={0}
+          y2={-88}
+          strokeWidth={3}
+          stroke="currentColor"
+          transform={`rotate(${minute * 6})`}
+        />
+        <line
+          x1={0}
+          y1={20}
+          x2={0}
+          y2={-93}
+          strokeWidth={1}
+          stroke="red"
+          transform={`rotate(${seconds * 6})`}
+        />
+        <circle
+          cx={0}
+          cy={0}
+          r={3}
+          fill="red"
+        />
+        {range(60).map(tick => (
+          <line
+            x1={0}
+            y1={tick % 5 ? -92 : -86}
+            x2={0}
+            y2={-95}
+            strokeWidth={tick % 5 ? 1 : 3}
+            stroke="currentColor"
+            transform={`rotate(${tick * 6})`}
+          />
+        ))}
+      </g>
+    </svg>
+  );
+};
+
+export default Clock;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,9 @@ import React from 'react';
 import fecha from 'fecha';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
-import { Button, ButtonToolbar, Card, CardBody } from 'reactstrap';
+import { Button, ButtonToolbar } from 'reactstrap';
 import Icon from 'react-fontawesome';
+import Clock from './Clock';
 
 @observer
 export default class Home extends React.Component {
@@ -12,7 +13,7 @@ export default class Home extends React.Component {
   componentDidMount() {
     this.interval = setInterval(() => {
       this.time = new Date();
-    }, 1000);
+    }, 100);
   }
 
   componentWillUnmount() {
@@ -25,15 +26,20 @@ export default class Home extends React.Component {
         <h1 className="border-dark border-bottom p-2">
           <Icon name="home" className="text-info" /> Home
         </h1>
-        <Card className="mb-3">
-          <CardBody>
+        <div className="d-flex align-items-center p-3">
+          <Clock time={this.time} />
+          <div className="pl-4">
             <h2>{fecha.format(this.time, 'h:mm:ssA')}</h2>
-            <h3 className="text-muted">{fecha.format(this.time, 'MMM Do, YYYY')}</h3>
-          </CardBody>
-        </Card>
+            <h3 className="text-muted">
+              {fecha.format(this.time, 'MMM Do, YYYY')}
+            </h3>
+          </div>
+        </div>
 
         <ButtonToolbar>
-          <Button color="primary" className="mr-1">Hello</Button>
+          <Button color="primary" className="mr-1">
+            Hello
+          </Button>
           <Button>World!</Button>
         </ButtonToolbar>
       </div>
